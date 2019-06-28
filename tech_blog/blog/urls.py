4e38f006cbdc69ac3a2f12b36905662306b55d96
sonicaks <adityakumarsinha2014@gmail.com>
2019-06-28 15:44:11 +0530

    Move accounts page from tech_blog/urls.py to blog/urls.py

--- tech_blog/blog/urls.py
+++ tech_blog/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'blog'
@@ -14,5 +15,7 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name = 'add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name = 'comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name = 'comment_remove'),
-    path('post/<int:pk>/publish/', views.post_publish, name = 'post_publish')
+    path('post/<int:pk>/publish/', views.post_publish, name = 'post_publish'),
+    path('accounts/login/', LoginView.as_view(), name = 'login'),
+    path('accounts/logout/', LogoutView.as_view(), name = 'logout', kwargs = {'next_page' : ''})
 ]
